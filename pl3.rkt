905be019270d9b9a97ff6597a7b32d99c55a6a91
Ryan Culpepper <ryanc@racket-lang.org>
2014-02-16 19:36:31 -0500

    call-site counter handles multiple modules

--- pl3.rkt
+++ pl3.rkt
@@ -5,11 +5,14 @@
                      syntax/parse
                      syntax/stx)
          racket/list
+         racket/match
          "private/context.rkt"
          "private/lib.rkt")
 (provide (except-out (all-from-out racket/base) #%module-begin #%top-interaction)
          (rename-out [instrumenting-module-begin #%module-begin]
                      [instrumenting-top-interaction #%top-interaction])
+         describe-all-call-sites
+         describe-call-site
          (all-from-out "private/lib.rkt"))
 
 #|
@@ -20,21 +23,52 @@ This module contains the implementation of the call-site instrumentor.
 
 #|
 A CallSite is a Nat.
---- counter of occurrences of #%plain-app syntax in expanded program
 
-Note: the counter resets for each new module, repl compilation.
-Can fix with gensym or pairing with module-path, etc, but no need for now.
-Will need to fix for real (multi-module) programs.
-Note: instrumenting-module? hack separates module from repl numbers.
+Call-site index determined dynamically, so distinct modules get
+distinct call-site indexes.
 |#
-(begin-for-syntax
-  (define instrumenting-module? (make-parameter #t))
-  (define counter 0)
-  (define (next-counter)
-    (set! counter (add1 counter))
-    (if (instrumenting-module?)
-        counter
-        (- counter))))
+
+;; call-site-counter : Nat
+(define call-site-counter 0)
+
+;; call-site-table : hash[nat => sexpr describing source]
+(define call-site-table (make-hash))
+
+;; next-counter : (List VariableReference any any any) : -> Nat
+(define (next-counter src-info)
+  (set! call-site-counter (add1 call-site-counter))
+  (hash-set! call-site-table call-site-counter src-info)
+  call-site-counter)
+
+(define-syntax (fresh-call-site stx)
+  (syntax-case stx ()
+    [(fresh-call-site call)
+     (with-syntax ([stx-file (syntax-source #'call)]
+                   [line (syntax-line #'call)]
+                   [col (syntax-column #'call)])
+       #'(next-counter
+          (cons (variable-reference->module-source (#%variable-reference))
+                '(stx-file line col call))))]))
+
+(define (describe-all-call-sites)
+  (for ([i (in-range 1 (add1 call-site-counter))])
+    (describe-call-site i)))
+
+(define (describe-call-site n)
+  (cond [(hash-ref call-site-table n #f)
+         => (lambda (info)
+              (match info
+                [(list mod src line col stx)
+                 (let ([mod (if (path? mod) (path->string mod) mod)]
+                       [src (if (path? src) (path->string src) src)])
+                   (printf "call site ~s: ~a:~a:~a\n"
+                           n
+                           (or src (and (not line) (not col) mod) "?")
+                           (or line "?")
+                           (or col "?")))]))]
+        [else (printf "call site ~s: no info available" n)]))
+
+;; ----
 
 (define-syntax (instrumenting-module-begin stx)
   (syntax-case stx ()
@@ -48,14 +82,13 @@ Note: instrumenting-module? hack separates module from repl numbers.
 (define-syntax (instrumenting-top-interaction stx)
   (syntax-case stx ()
     [(instrumenting-top-interaction . e)
-     (parameterize ((instrumenting-module? #f))
-       (let ([estx (local-expand #'(#%top-interaction . e) 'top-level #f)])
-         (syntax-case estx (begin)
-           [(begin form ...)
-            #'(begin (instrumenting-top-interaction . form) ...)]
-           [form
-            (with-syntax ([e-form (local-expand #'form 'top-level null)])
-              #'(instrument e-form #:un))])))]))
+     (let ([estx (local-expand #'(#%top-interaction . e) 'top-level #f)])
+       (syntax-case estx (begin)
+         [(begin form ...)
+          #'(begin (instrumenting-top-interaction . form) ...)]
+         [form
+          (with-syntax ([e-form (local-expand #'form 'top-level null)])
+            #'(instrument e-form #:un))]))]))
 
 (begin-for-syntax
  ;; Need privileged inspector to rewrite expanded code.
@@ -91,20 +124,25 @@ Note: instrumenting-module? hack separates module from repl numbers.
                    (when (and (eq? classification 'unknown)
                               (not (eq? (syntax->datum #'m) '#:nt)))
                      (eprintf "## ~s\n" (syntax-e #'f))))
-                 (with-syntax ([c (next-counter)])
-                   #'(app/call-site* m 'c f (instrument e #:nt) ...))])]
+                 (with-syntax ([c (syntax-local-lift-expression
+                                   #`(fresh-call-site #,stx))])
+                   #'(app/call-site* m c f (instrument e #:nt) ...))])]
          [(#%plain-app f e ...)
-          (with-syntax ([c (next-counter)])
+          (with-syntax ([c (syntax-local-lift-expression
+                            #`(fresh-call-site #,stx))])
             (when #f
               (when (not (eq? (syntax->datum #'m) '#:nt))
                 (eprintf "## ~s\n" (syntax->datum #'f))))
+            #|
+            ;; Use call-site-registry instead.
             (when #f
               (eprintf "app ~s = ~a:~a ~.s\n\n"
                        (syntax-e #'c)
                        (or (syntax-line stx) '?)
                        (or (syntax-column stx) '?)
                        (syntax->datum stx)))
-            #'(app/call-site* m 'c (instrument f #:nt) (instrument e #:nt) ...))]
+            |#
+            #'(app/call-site* m c (instrument f #:nt) (instrument e #:nt) ...))]
          ;; Just recur through all other forms
          ;; -- module body
          [(#%plain-module-begin form ...)
