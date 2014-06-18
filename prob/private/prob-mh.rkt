d628b30bc2f5d7442cb8b9d622a37318e1117971
Ryan Culpepper <ryanc@racket-lang.org>
2014-06-18 14:28:52 -0400

    add perturb-mode field

--- prob/private/prob-mh.rkt
+++ prob/private/prob-mh.rkt
@@ -88,6 +88,11 @@ depending on only choices reused w/ different params.
 ;; - 'purge : same, but purge unused entries from db
 (define default-threshold-mode 'retain)
 
+;; default-perturb-mode : list containing some subset of the following
+;; - 'normal : use specialized proposal distribution for normal dist
+;; (the default is to simply resample)
+(define default-perturb-mode '(normal))
+
 ;; ----
 
 (define (mh-sampler* thunk spconds)
@@ -101,6 +106,7 @@ depending on only choices reused w/ different params.
            [accepts 0]
            [cond-rejects 0]
            [mh-rejects 0]
+           [perturb-mode default-perturb-mode]
            [reject-mode default-reject-mode]
            [threshold-mode default-threshold-mode]
            [last-sample #f])
@@ -162,33 +168,48 @@ depending on only choices reused w/ different params.
          (reject)]))
 
     ;; perturb! : DB Address -> Real
+    ;; Updates db and returns (log) R-F.
     (define/private (perturb! db key-to-change)
       (match (hash-ref db key-to-change)
         [(entry tag dist value #f)
-         ;; update! : ... -> Real
-         (define (update! R F value*)
-           (when (verbose?)
-             (eprintf "  from ~e to ~e\n" value value*)
-             (eprintf "  R = ~s, F = ~s\n" (exp R) (exp F)))
-           (hash-set! db key-to-change (entry tag dist value* #f))
-           (- R F))
-         (match tag
-           [`(normal ,mean ,stddev)
-            (define forward-dist
-              (make-normal-dist value (/ stddev 4.0)))
-            (define value* (dist-sample forward-dist))
-            (define backward-dist
-              (make-normal-dist value* (/ stddev 4.0)))
-            (define R (dist-pdf backward-dist value #t))
-            (define F (dist-pdf forward-dist value* #t))
-            (update! R F value*)]
-           ;; FIXME: insert specialized proposal distributions here
-           [_ ;; Fallback: Just resample from same dist.
-            ;; Then Kt(x|x') = Kt(x) = (dist-pdf dist value)
-            ;;  and Kt(x'|x) = Kt(x') = (dist-pdf dist value*)
-            ;; All cancel, so just pass 0 for all.
-            (update! 0 0 (dist-sample dist))])]))
+         (or (perturb!/proposal db key-to-change tag dist value)
+             ;; Fallback: Just resample from same dist.
+             ;; Then Kt(x|x') = Kt(x) = (dist-pdf dist value)
+             ;;  and Kt(x'|x) = Kt(x') = (dist-pdf dist value*)
+             ;; All cancel, so just 0 for R-F.
+             (let ([value* (dist-sample dist)])
+               (when (verbose?)
+                 (eprintf "  RESAMPLED from ~e to ~e\n" value value*))
+               (hash-set! db key-to-change (entry tag dist value* #f))
+               0))]))
 
+    ;; perturb!/proposal : DB Address Tag Dist Any -> (U Real #f)
+    ;; If applicable proposal dist avail, updates db and returns (log) R-F,
+    ;; otherwise returns #f and perturb! should just resample.
+    (define/private (perturb!/proposal db key-to-change tag dist value)
+      ;; update! : ... -> Real
+      (define (update! R F value*)
+        (when (verbose?)
+          (eprintf "  PROPOSED from ~e to ~e\n" value value*)
+          (eprintf "  R = ~s, F = ~s\n" (exp R) (exp F)))
+        (hash-set! db key-to-change (entry tag dist value* #f))
+        (- R F))
+      (match tag
+        [`(normal ,mean ,stddev)
+         (and (memq 'normal perturb-mode)
+              (let ()
+                (define forward-dist
+                  (make-normal-dist value (/ stddev 4.0)))
+                (define value* (dist-sample forward-dist))
+                (define backward-dist
+                  (make-normal-dist value* (/ stddev 4.0)))
+                (define R (dist-pdf backward-dist value #t))
+                (define F (dist-pdf forward-dist value* #t))
+                (update! R F value*)))]
+        [_ #f]))
+
+    ;; accept-threshold : ... -> Real
+    ;; Computes (log) accept threshold for current trace.
     (define/private (accept-threshold R-F current-db maybe-prev-db key-to-change)
       (if (and maybe-prev-db (positive? (hash-count current-db)))
           (accept-threshold* R-F current-db maybe-prev-db key-to-change)
