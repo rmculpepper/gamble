a8135578ec5ab653afc153241f81b42347f7eded
Ryan Culpepper <ryanc@racket-lang.org>
2014-06-24 12:45:13 -0400

    add missing copyright notices

--- prob/private/pairingheap.rkt
+++ prob/private/pairingheap.rkt
@@ -1,3 +1,7 @@
+;; Copyright (c) 2014 Ryan Culpepper
+;; Released under the terms of the 2-clause BSD license.
+;; See the file COPYRIGHT for details.
+
 ;; Adapted from (planet krhari/pfds:1:5/pairingheap)
 ;; http://planet.racket-lang.org/package-source/krhari/pfds.plt/1/5/
 
