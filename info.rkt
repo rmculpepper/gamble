34dc2a56c1b4b3c65436cd6bb112b03af9c01a75
Ryan Culpepper <ryanc@racket-lang.org>
2014-06-24 14:29:59 -0400

    add pkg deps

--- info.rkt
+++ info.rkt
@@ -5,3 +5,18 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+(define deps '("base"
+               "data-lib"
+               "math-lib"
+               "gui-lib"
+               "plot-lib"
+               "plot-gui-lib"
+               "pict-lib"
+               "rackunit-lib"
+               "typed-racket-lib"))
+(define build-deps '("racket-doc"
+                     "scribble-lib"
+                     "pict-doc"
+                     "plot-doc"
+                     "math-doc"))
