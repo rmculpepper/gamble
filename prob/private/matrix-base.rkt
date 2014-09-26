d123dc89fc27fad6ca754bd13f0bde59e8563b76
Ryan Culpepper <ryanc@racket-lang.org>
2014-09-26 12:44:10 -0400

    allow disabling printing of matrix wrapper struct
    Not runtime configurable (grr), and disabled for now
    (until matrix code settles down).

--- prob/private/matrix-base.rkt
+++ prob/private/matrix-base.rkt
@@ -7,7 +7,6 @@
 #lang typed/racket/base
 (require (prefix-in t: math/array)
          (prefix-in t: math/matrix))
-
 (provide (struct-out ImmArray)
          (struct-out MutArray)
          Array
@@ -17,8 +16,48 @@
          ImmMatrix
          MutMatrix)
 
-(struct: ImmArray ([contents : (t:Array Real)]) #:transparent)
-(struct: MutArray ([contents : (t:Mutable-Array Real)]) #:transparent)
+;; ----------------------------------------
+;; Printing
+
+;; Wrapper printing is currently INCLUDED.
+
+;; To OMIT the printing of the wrapper struct, make sure the
+;;     #:property prop:custom-write ....
+;; lines in the struct definitions below are UNCOMMENTED.
+;; 
+;; To INCLUDE the printing of the wrapper struct, make sure the
+;;     #:property prop:custom-write ....
+;; lines in the struct definitions below are COMMENTED OUT.
+;;
+;; (Various factors conspire to make it obnoxious to make this
+;; configurable more gracefully, eg at run time.)
+
+(: print-imm : Any Output-Port (U #t #f 0 1) -> Any)
+(define print-imm
+  (lambda (imm out mode)
+    (print-recur (ImmArray-contents (cast imm ImmArray)) out mode)))
+
+(: print-mut : Any Output-Port (U #t #f 0 1) -> Any)
+(define print-mut
+  (lambda (mut out mode)
+    (print-recur (MutArray-contents (cast mut MutArray)) out mode)))
+
+(: print-recur : Any Output-Port (U #t #f 0 1) -> Any)
+(define (print-recur v out mode)
+  (cond [(not mode) (display v out)]
+        [(integer? mode) (print v out mode)]
+        [else (write v out)]))
+
+;; ----------------------------------------
+
+(struct: ImmArray ([contents : (t:Array Real)])
+         #:transparent
+         ;; #:property prop:custom-write print-imm
+         #:property prop:custom-print-quotable 'never)
+(struct: MutArray ([contents : (t:Mutable-Array Real)])
+         #:transparent
+         ;; #:property prop:custom-write print-mut
+         #:property prop:custom-print-quotable 'never)
 (define-type Array (U ImmArray MutArray))
 
 (define (Array? x)
