e80119d501ceba9e0d91d460fcd5aea37b528946
Ryan Culpepper <ryanc@racket-lang.org>
2015-01-21 17:02:07 -0500

    clean up observe tests

--- prob/tests/observe.rkt
+++ prob/tests/observe.rkt
@@ -12,33 +12,31 @@
 
 (define CD-EPSILON 0.1)
 
-;; sample in tail position wrt observe
-(define s 
-  (importance-sampler 
-   (define R (normal 10 1))
-   (define (S) (normal R 1))
-   (observe (S) 9)
-   R))
+(test-case "sample in tail position wrt observe"
+  (define s
+    (importance-sampler
+     (define R (normal 10 1))
+     (define (S) (normal R 1))
+     (observe (S) 9)
+     R))
+  (let ([mean (sampler->mean s 1000)])
+    (check-= mean 9.5 CD-EPSILON)))
 
-;; sample in (+ v []) wrt observe
-(define s2
-  (importance-sampler
-   (define R (normal 10 1))
-   (define (S) (+ R (normal 0 1)))
-   (observe (S) 9)
-   R))
-
-(let ([mean (sampler->mean s 1000)])
-  (check-= mean 9.5 CD-EPSILON))
-(let ([mean (sampler->mean s2 1000)])
-  (check-= mean 9.5 CD-EPSILON))
+(test-case "observe propagated through +"
+  (define s2
+    (importance-sampler
+     (define R (normal 10 1))
+     (define (S) (+ R (normal 0 1)))
+     (observe (S) 9)
+     R))
+  (let ([mean (sampler->mean s2 1000)])
+    (check-= mean 9.5 CD-EPSILON)))
 
 ;; ----
 
 ;; observation scaling
 
-(let ()
-  ;; Continuous observation; must scale
+(test-case "obs on continuous dist scaled"
   (define EPSILON 0.1)
   (define s
     (importance-sampler
@@ -49,8 +47,7 @@
   (define m (sampler->mean s 1000))
   (check-= (sampler->mean s 1000 (indicator/value #t)) 0.5 EPSILON))
 
-(let ()
-  ;; Discrete observation; must not scale
+(test-case "obs on discrete dist not scaled"
   (define EPSILON 0.1)
   (define s
     (importance-sampler
@@ -62,21 +59,28 @@
 
 ;; ----
 
-;; Works through contracts (matrix)
-(let ()
+;; can't be submodule due to racket expander bug (?)
+(require (only-in "observe-helper.rkt" add1/ctc))
+
+(test-case "obs propagated through contracted op (simple)"
+  (define s
+    (mh-sampler
+     (observe (add1/ctc (bernoulli)) 1)
+     'ok))
+  (for ([i 100]) (check-equal? (s) 'ok)))
+
+(test-case "obs propagated through contracted op (matrix)"
   (define s
     (mh-sampler
      (define a (multi-normal (->col-matrix '(0 0)) (identity-matrix 2)))
      (define (b) (matrix* (matrix [[2 1][1 2]]) (multi-normal a (identity-matrix 2))))
      (observe (b) (matrix [[2.3][1.7]]))
      a))
-  (void (sampler->mean s 1000)))
+  (check-pred matrix? (sampler->mean s 1000)))
 
 ;; ----
 
-;; observation inversion type error -> fail, not error
-
-(let ()
+(test-case "obs inversion type error -> fail, not error"
   (define s
     (mh-sampler
      (define a (flip))
@@ -88,39 +92,41 @@
 
 ;; ----
 
-;; conditioning on lists
-
-(define s-list
-  (importance-sampler
-   ;; note: annotated version of build-list from prob/private/ho-functions
-   (define (data) (build-list 3 (lambda (_i) (bernoulli 1/2))))
-   (observe (data) '(0 0 0))))
-
-(check-equal? (send s-list sample/weight)
-              '((0 0 0) . 1/8))
+(test-case "observe flip"
+  (define s (mh-sampler (observe (flip) #t) 'ok))
+  (for ([i 100]) (check-equal? (s) 'ok)))
 
 ;; ----
 
-;; works in enumerate
-(check-equal? (enumerate (observe (+ 1 (bernoulli)) 2))
-              (discrete-dist [2 1]))
+(test-case "conditioning on lists"
+  (define s-list
+    (importance-sampler
+     ;; note: annotated version of build-list from prob/private/ho-functions
+     (define (data) (build-list 3 (lambda (_i) (bernoulli 1/2))))
+     (observe (data) '(0 0 0))))
+  (check-equal? (send s-list sample/weight)
+                '((0 0 0) . 1/8)))
 
 ;; ----
 
-;; works through mem
+(test-case "observe works in enumerate"
+  (check-equal? (enumerate (observe (+ 1 (bernoulli)) 2))
+                (discrete-dist [2 1])))
 
-(define (mem-test)
-  (define f (mem (lambda (i) (bernoulli))))
-  (for ([i 10]) (observe (f i) 0))
-  (for/and ([i 10]) (= (f i) 0)))
+;; ----
 
-(check-equal? (mem-test) #t)
-(check-equal? (enumerate (mem-test))
-              (discrete-dist [#t 1]))
-(check-equal? ((mh-sampler (mem-test)))
-              #t)
-(check-equal? (send (importance-sampler (mem-test)) sample/weight)
-              (cons #t (expt 1/2 10)))
+(test-case "observe works through mem"
+  (define (mem-test)
+    (define f (mem (lambda (i) (bernoulli))))
+    (for ([i 10]) (observe (f i) 0))
+    (for/and ([i 10]) (= (f i) 0)))
+  (check-equal? (mem-test) #t)
+  (check-equal? (enumerate (mem-test))
+                (discrete-dist [#t 1]))
+  (check-equal? ((mh-sampler (mem-test)))
+                #t)
+  (check-equal? (send (importance-sampler (mem-test)) sample/weight)
+                (cons #t (expt 1/2 10))))
 
 ;; ----
 
@@ -131,17 +137,19 @@
 
 ;; check-observe -- used to check parts of expected value early
 
-(check-not-exn
- (lambda ()
-   (observe (cons (check-observe 1)
-                  (cons (check-observe 2)
-                        null))
-            '(1 2))))
+(test-case "check-observe 1"
+  (check-not-exn
+   (lambda ()
+     (observe (cons (check-observe 1)
+                    (cons (check-observe 2)
+                          null))
+              '(1 2)))))
 
-(check-exn
- #rx"check-observe:.*expected: 1.*got: 0"
- (lambda ()
-   (observe (cons (check-observe 0) ;; !!!
-                  (cons (check-observe 2)
-                        null))
-            '(1 2))))
+(test-case "check-observe 2"
+  (check-exn
+   #rx"check-observe:.*expected: 1.*got: 0"
+   (lambda ()
+     (observe (cons (check-observe 0) ;; !!!
+                    (cons (check-observe 2)
+                          null))
+              '(1 2)))))
