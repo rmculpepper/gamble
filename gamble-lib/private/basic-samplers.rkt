f6787718a1e5ac0a242e4cd43490e8dda8462887
Ryan Culpepper <ryanc@racket-lang.org>
2020-07-30 14:06:35 +0200

    ... basic samplers

---
+++ gamble-lib/private/basic-samplers.rkt
@@ -0,0 +1,163 @@
+;; Copyright 2014-2020 Ryan Culpepper
+;; Released under the terms of the 2-clause BSD license.
+;; See the file COPYRIGHT for details.
+
+#lang racket/base
+(require (for-syntax racket/base syntax/parse)
+         racket/match
+         racket/class
+         "dist/base.rkt"
+         "util/debug.rkt"
+         "interfaces.rkt")
+(provide (all-defined-out))
+
+;; ============================================================
+;; Rejection sampling
+
+(define-syntax (rejection-sampler stx)
+  (syntax-parse stx
+    [(rejection-query def:expr ... result:expr)
+     #'(rejection-sampler* (lambda () def ... result))]))
+
+(define (rejection-sampler* thunk)
+  (new rejection-sampler% (thunk thunk)))
+
+(define rejection-sampler%
+  (class sampler-base%
+    (init-field thunk)
+    (field [successes 0]
+           [rejections 0])
+    (super-new)
+
+    (define/override (info)
+      (printf "== Rejection sampler\n")
+      (printf "Samples produced: ~s\n" successes)
+      (printf "Rejections: ~s\n" rejections))
+
+    (define/override (sample)
+      (define ctx (new rejection-stochastic-ctx%))
+      (define v (send ctx run thunk))
+      (case (car v)
+        [(okay)
+         (set! successes (add1 successes))
+         (cdr v)]
+        [(fail)
+         (set! rejections (add1 rejections))
+         (sample)]))
+    ))
+
+(define rejection-stochastic-ctx%
+  (class plain-stochastic-ctx/run%
+    (inherit fail run)
+    (super-new)
+
+    (define/private (not-supported who [more ""])
+      (error who "not supported by rejection sampler~a" more))
+
+    (define/override (lscore ll)
+      (not-supported 'lscore))
+    (define/override (nscore l)
+      (not-supported 'nscore))
+
+    (define/override (observe dist val scale)
+      (define-values (d ddim) (dist-density dist val #t))
+      (cond [(zero? ddim)
+             ;; FIXME: assumes total weight is 1!
+             (unless (< (log (random)) d)
+               (fail 'observation))]
+            [else (not-supported 'observe (format "\n  distribution: ~e" dist))]))
+
+    (define/public (trycatch p1 p2)
+      (match (run p1)
+        [(cons 'okay value)
+         value]
+        [(cons 'fail _)
+         (p2)]))
+    ))
+
+;; ============================================================
+;; Importance sampling
+
+(define-syntax (importance-sampler stx)
+  (syntax-parse stx
+    [(importance-sampler def:expr ... result:expr)
+     #'(importance-sampler*
+        (lambda () def ... result))]))
+
+(define (importance-sampler* thunk)
+  (new importance-sampler% (thunk thunk)))
+
+(define importance-sampler%
+  (class* object% (weighted-sampler<%>)
+    (init-field thunk)
+    (field [successes 0]
+           [rejections 0]
+           [dens-dim-rejections 0]
+           [min-dens-dim +inf.0]
+           [bad-samples 0])
+    (super-new)
+
+    (define/public (info)
+      (printf "== Importance sampler\n")
+      (printf "Samples produced: ~s\n" successes)
+      (printf "Rejections: ~s\n" rejections)
+      (printf "Density dimension: ~s\n" min-dens-dim)
+      (unless (zero? dens-dim-rejections)
+        (printf "Density dimension rejections: ~s\n" dens-dim-rejections))
+      (unless (zero? bad-samples)
+        (printf "Bad samples emitted (wrong density dimension): ~s" bad-samples)))
+
+    (define/public (sample/weight)
+      (define ctx (new importance-stochastic-ctx%))
+      (define v (send ctx run thunk))
+      (case (car v)
+        [(okay)
+         (define dens-dim (get-field dens-dim ctx))
+         (when (< dens-dim min-dens-dim)
+           (unless (zero? successes)
+             (eprintf "WARNING: previous ~s samples are meaningless; wrong density dimension\n"
+                      successes))
+           (vprintf "Lower density dimension seen: ~s\n" dens-dim)
+           (set! bad-samples successes)
+           (set! min-dens-dim dens-dim))
+         (cond [(<= dens-dim min-dens-dim)
+                (set! successes (add1 successes))
+                (cons (cdr v) (get-field weight ctx))]
+               [else
+                (set! dens-dim-rejections (add1 dens-dim-rejections))
+                (set! rejections (add1 rejections))
+                (sample/weight)])]
+        [(fail)
+         (set! rejections (add1 rejections))
+         (sample/weight)]))
+    ))
+
+(define importance-stochastic-ctx%
+  (class rejection-stochastic-ctx%
+    (field [weight 1]
+           [obs-ddim 0])
+    (inherit fail run)
+    (super-new)
+
+    (define/override (lscore ll)
+      (nscore (exp ll) 'lscore))
+    (define/override (nscore l [who 'nscore])
+      (set! weight (* weight l))
+      (unless (positive? l) (fail who)))
+
+    (define/override (observe dist val scale)
+      (define-values (d ddim) (dist-density dist val))
+      (unless (zero? ddim) (set! obs-ddim (add1 obs-ddim)))
+      (nscore d 'observe))
+
+    (define/override (trycatch p1 p2)
+      (define saved-weight weight)
+      (define saved-obs-ddim obs-ddim)
+      (match (run p1)
+        [(cons 'okay value)
+         value]
+        [(cons 'fail _)
+         (set! weight saved-weight)
+         (set! obs-ddim saved-obs-ddim)
+         (p2)]))
+    ))
