d3f287c98f66ef2c2d4e1ef509a0f28d649e8b85
Ryan Culpepper <ryanc@racket-lang.org>
2025-06-03 00:00:56 -0400

    dist: add checks to base, delete old comments

--- gamble-lib/private/dist/base.rkt
+++ gamble-lib/private/dist/base.rkt
@@ -53,11 +53,22 @@
            [(integer-dist? self) 1]
            [else (raise-support-error 'dist-total-measure self)]))])
 
-(define (dist-sample d) (-sample d))
-(define (dist-pdf d x [log? #f]) (-pdf d x log?))
-(define (dist-density d x [log? #f]) (-density d x log?))
-(define (dist-measure d ms) (-measure d ms))
-(define (dist-total-measure d) (-total-measure d))
+(define (dist-sample d)
+  (unless (dist? d) (raise-argument-error 'dist-sample "dist?" d))
+  (-sample d))
+(define (dist-pdf d x [log? #f])
+  (unless (dist? d) (raise-argument-error 'dist-pdf "dist?" d))
+  (-pdf d x (and log? #t)))
+(define (dist-density d x [log? #f])
+  (unless (dist? d) (raise-argument-error 'dist-density "dist?" d))
+  (-density d x (and log? #t)))
+(define (dist-measure d ms)
+  (unless (dist? d) (raise-argument-error 'dist-measure "dist?" d))
+  (unless (measurable? ms) (raise-argument-error 'dist-measure "measurable?" ms))
+  (-measure d ms))
+(define (dist-total-measure d)
+  (unless (dist? d) (raise-argument-error 'dist-total-measure "dist?" d))
+  (-total-measure d))
 
 (define-generics enumerable-dist   ;; extends dist
   ;; Represents discrete, enumerable distributions.
@@ -68,8 +79,12 @@
      (sequence-map (lambda (v) (values v (dist-pdf self v #f)))
                    (in-dist-values self)))])
 
-(define (in-dist dist) (-wsequence dist))
-(define (in-dist-values dist) (-sequence dist))
+(define (in-dist d)
+  (unless (enumerable-dist? d) (raise-argument-error 'in-dist "enumerable-dist?" d))
+  (-wsequence d))
+(define (in-dist-values d)
+  (unless (enumerable-dist? d) (raise-argument-error 'in-dist-values "enumerable-dist?" d))
+  (-sequence d))
 
 (define-generics real-dist ;; extends dist; comprises {continuous,integer}-dist
   ;; Represents normalized real-valued distributions.
@@ -82,11 +97,6 @@
   (-median real-dist)              ;; Dist -> Real or #f
   (-modes real-dist)               ;; Dist -> (Listof Real) or #f
   (-variance real-dist)            ;; Dist -> Real or #f
-  #:defaults
-  ([(lambda (v) (continuous-dist? v))
-    (define (-ddim self) 1)]
-   [(lambda (v) (integer-dist? v))
-    (define (-ddim self) 0)])
   #:fallbacks
   [(define (-support d) '(-inf.0 . +inf.0))
    (define (-mean d) #f)
@@ -95,12 +105,16 @@
    (define (-variance d) #f)])
 
 (define (dist-cdf d x [log? #f] [1-p? #f])
-  (-cdf d x log? 1-p?))
+  (unless (real-dist? d) (raise-argument-error 'dist-cdf "real-dist?" d))
+  (unless (real? x) (raise-argument-error 'dist-cdf "real?" x))
+  (-cdf d x (and log? #t) (and 1-p? #t)))
 
 ;; dist-inv-cdf : Real-Dist Real Boolean Boolean -> Real
 ;; (dist-inv-cdf d p) returns least x such that Pr[X <= x] >= p, where X ~ d.
 (define (dist-inv-cdf d p [log? #f] [1-p? #f])
-  (-invcdf d p log? 1-p?))
+  (unless (real-dist? d) (raise-argument-error 'dist-inv-cdf "real-dist?" d))
+  (unless (real? p) (raise-argument-error 'dist-inv-cdf "real?" p))
+  (-invcdf d p (and log?) (and 1-p?)))
 
 (define-generics continuous-dist   ;; extends real-dist
   ;; Represents normalized, continuous real-valued distributions.
@@ -111,146 +125,11 @@
   ;; Represents discrete, normalized integer-valued distributions.
   #:fallbacks [])
 
-;; FIXME: real^2-dist
-;; FIXME: real^n-dist
-
 ;; ============================================================
-
-;; (define-generics dist/sample
-;;   ;; type X
-;;   (*sample dist/sample) ;; Dist -> X
-;;   (*wsample dist/sample) ;; Dist -> (values X NNReal/#f LogReal) -- FIXME, Density?
-;;   #:fallbacks
-;;   [(define (*wsample self) (values (dist-sample self) 1 0.0))])
-
-;; (define (dist-sample d)
-;;   (*sample d))
-
-;; (define-generics dist
-;;   ;; type X    -- element type
-
-;;   ;; Meta
-;;   (*type dist)                ; Dist -> Symbol
-;;   (*params dist)              ; Dist -> (vector Param ...)
-
-;;   ;; Density/mass
-;;   ;; density/pdf accepts any value, gives 0/-inf.0 if not in support
-;;   ;; cdf, inv-cdf may raise error if not in support
-;;   (*density dist x full?)     ; Dist X Boolean -> Density
-;;   (*pdf dist x log?)          ; Dist X Boolean -> Real -- beware, omits ddim!
-;;   (*cdf dist x log? 1-p?)     ; Dist X Boolean Boolean -> Real (or error)
-;;   (*inv-cdf dist x log? 1-p?) ; Dist Real Boolean Boolean -> X (or error)
-;;   (*total-mass dist)          ; Dist -> NNReal or #f if unknown
-;;   (*support dist)             ; Dist -> DistSupport
-;;   (*Denergy dist x . d/dts)   ; Dist X Param ... -> Real
-;;   (*enum dist)                ; Dist -> #f or ??
-;;   (*conjugate dist ddist data); Dist KernelSexpr Any -> Dist/#f
-
-;;   ;; Real-valued dists (X = Real)
-;;   (*mean dist)                ; Dist -> Real/#f/NaN
-;;   (*median dist)              ; Dist -> Real/#f/NaN
-;;   (*modes dist)               ; Dist -> (Listof Real) or #f if unknown
-;;   (*variance dist)            ; Dist -> Real/#f/NaN
-
-;;   #:fallbacks
-;;   [;; Density/mass
-;;    (define (*pdf d x log?)
-;;      (define dn (dist-density d x))
-;;      (if log? (density-l dn) (density-n dn)))
-;;    (define (*cdf d x log? 1-p?)
-;;      (error 'dist-cdf "not defined for distribution\n  given: ~e" d))
-;;    (define (*inv-cdf d x log? 1-p?)
-;;      (error 'dist-inv-cdf "not defined for distribution\n  given: ~e" d))
-;;    (define (*total-mass d) #f)
-;;    (define (*support d) #f)
-;;    (define (*Denergy d x . d/dts) #f)
-;;    (define (*enum d) #f)
-;;    (define (*conjugate d data-d data) #f)
-;;    ;; Real dists
-;;    (define (*mean d) #f)
-;;    (define (*median d) #f)
-;;    (define (*modes d) #f)
-;;    (define (*variance d) #f)])
-
-;; (define (dists-same-type? da db)
-;;   (equal? (*type da) (*type db)))
-
-;; (define (dist-density d x [full? #t])
-;;   (*density d x full?))
-;; (define (dist-density* d xs [full? #t])
-;;   (density-product (for/list ([x xs]) (dist-density d x full?))))
-;; (define (dist-pdf d x [log? #f])
-;;   (*pdf d x log?))
-;; (define (dist-cdf d x [log? #f] [1-p? #f])
-;;   (*cdf d x log? 1-p?))
-;; (define (dist-inv-cdf d x [log? #f] [1-p? #f])
-;;   (*inv-cdf d x log? 1-p?))
-
-;; (define (dist-total-mass d)
-;;   (*total-mass d))
-
-;; (define (dist-energy d x) ;; Energy = -log(pdf(d,x))
-;;   (- (dist-pdf d x #t)))
-;; (define (dist-Denergy d x . d/dts)
-;;   ;; derivative of energy(d,x) wrt t, treating x and params(d) as functions of t
-;;   ;; d/dts = dx/dt (default 1), dparam1/dt (default 0), ...
-;;   (apply *Denergy d x d/dts))
-
-;; (define (dist-support d)
-;;   (*support d))
-
-;; (define (dist-enum d)
-;;   (*enum d))
 
 ;; ;; DistSupport is one of
 ;; ;; - #f        -- unknown/unrestricted
-;; ;; - 'finite   -- unknown but finite
 ;; ;; - #s(integer-range Min Max)  -- inclusive
 ;; ;; - #s(real-range Min Max)     -- inclusive (may overapprox)
-;; ;; - TODO: #s(product (Vectorof Support)), ...
-;; (struct integer-range (min max) #:prefab)
-;; (struct real-range (min max) #:prefab)
-
-;; ;; Returns #t if dist is necessarily {integer,real}-valued.
-;; ;; Note: a discrete-dist that happens to have integer values is NOT integer-dist?.
-;; (define (integer-dist? d)
-;;   (and (dist? d) (integer-range? (dist-support d))))
-;; (define (real-dist? d)
-;;   (and (dist? d) (real-range? (dist-support d))))
-;; (define (finite-dist? d)
-;;   (define support (and (dist? d) (dist-support d)))
-;;   (or (eq? support 'finite)
-;;       (and (integer-range? support)
-;;            (> (integer-range-min support) -inf.0)
-;;            (< (integer-range-max support) +inf.0))))
-
-;; ;; FIXME: also compute normalizing constant (as density)
-;; (define (dist-conjugate d data-d data)
-;;   (or (*conjugate d data-d data)
-;;       (error 'dist-conjugate "combination not supported\n  dist: ~e\n  kernel: ~e\n  data: ~e"
-;;              d data-d data)))
-
-;; ;; dist-{mean,median,variance} : Dist -> Real | #f | NaN
-;; ;; #f means unknown; NaN means known to be undefined
-;; (define (dist-mean d)     (*mean d))
-;; (define (dist-median d)   (*median d))
-;; (define (dist-variance d) (*variance d))
-
-;; ;; dist-modes : Dist -> (Listof Real) | #f
-;; (define (dist-modes d)    (*modes d))
-
-#;
-(define (finite-dist->hash who d)
-  (cond [(discrete-dist? d)
-         (discrete-dist-h d)]
-        [(dist-enum d)
-         => (lambda (enum)
-              (cond [(integer? enum)
-                     (for/fold ([h (hash)]) ([i (in-range enum)])
-                       (hash-set h i (dist-pdf d i)))]
-                    [(vector? enum)
-                     (for/fold ([dh (hash)]) ([v (in-vector enum)])
-                       (dhash-add dh v (dist-pdf d v)))]
-                    [else
-                     (error who "internal error: non-enumerable finite dist\n  dist: ~e" d)]))]
-        [else (raise-argument-error who "finite-dist?" d)]))
+(struct integer-range (lo hi) #:prefab)
+(struct real-range (lo hi) #:prefab)
