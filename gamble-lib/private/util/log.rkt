d135eb53ae9fe7a096ae6399e97e842fc39e8fe9
Ryan Culpepper <ryanc@racket-lang.org>
2025-06-08 21:55:16 -0400

    util: add logging support

---
+++ gamble-lib/private/util/log.rkt
@@ -0,0 +1,8 @@
+;; Copyright (c) 2016 Ryan Culpepper
+;; Released under the terms of the 2-clause BSD license.
+;; See the file COPYRIGHT for details.
+
+#lang racket/base
+(provide (all-defined-out))
+
+(define-logger gamble)
