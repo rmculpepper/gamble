a03fb00f96066123e71c18cebf3e1137e4c711df
Ryan Culpepper <ryanc@racket-lang.org>
2025-06-12 12:40:13 -0400

    gamble-lib: add info.rkt

---
+++ gamble-lib/info.rkt
@@ -0,0 +1,18 @@
+#lang info
+
+;; pkg info
+
+(define version "2.0")
+(define collection "gamble")
+(define deps
+  '("base"
+    "math-lib"
+    "rackunit-lib"
+    "scramble-lib"
+    "typed-racket-lib"))
+(define pkg-authors '(ryanc))
+(define license '(BSD-2-Clause))
+
+;; collection info
+
+(define name "gamble")
